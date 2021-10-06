25c8c7499e979edd4c2aa3a23b0793a8ceb1120e
tyfahpundo <tafadzwaallentyfah@gmail.com>
2021-10-06 09:20:47 +0200

    Initial Commit

---
+++ src/test/java/zw/co/afrosoft/studentservice/StudentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package zw.co.afrosoft.studentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
